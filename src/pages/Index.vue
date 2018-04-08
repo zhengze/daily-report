8fb737a70ccb1bb210369b973984b2032a3f25d2
zhengze <zhanghhd@163.com>
2018-04-08 18:48:00 +0800

    eslint fix

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -26,19 +26,19 @@
         <el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
            unique-opened router v-show="!collapsed">
           <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-            <el-submenu :index="index+''" v-if="!item.leaf">
+            <el-submenu :index="index+''" :key="index" v-if="!item.leaf">
               <template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
               <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
             </el-submenu>
-            <el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
+            <el-menu-item v-if="item.leaf&&item.children.length>0" :key="item" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
           </template>
         </el-menu>
         <!--导航菜单-折叠后-->
         <ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
-          <li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
+          <li v-for="(item,index) in $router.options.routes" :key="index" v-if="!item.hidden" class="el-submenu item">
             <template v-if="!item.leaf">
               <div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"><i :class="item.iconCls"></i></div>
-              <ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"> 
+              <ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
                 <li v-for="child in item.children" v-if="!child.hidden" :key="child.path" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">{{child.name}}</li>
               </ul>
             </template>
@@ -72,69 +72,66 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        sysName:'DAILY REPORT',
-        collapsed:false,
-        sysUserName: '',
-        sysUserAvatar: '',
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
+export default {
+  data () {
+    return {
+      sysName: 'DailyReport',
+      collapsed: false,
+      sysUserName: '',
+      sysUserAvatar: '',
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
       }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
     },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
-      },
-      handleopen() {
-        //console.log('handleopen');
-      },
-      handleclose() {
-        //console.log('handleclose');
-      },
-      handleselect: function (a, b) {
-      },
-      //退出登录
-      logout: function () {
-        var _this = this;
-        this.$confirm('确认退出吗?', '提示', {
-          //type: 'warning'
-        }).then(() => {
-          sessionStorage.removeItem('user');
-          _this.$router.push('/login');
-        }).catch(() => {
-
-        });
-
-
-      },
-      //折叠导航栏
-      collapse:function(){
-        this.collapsed=!this.collapsed;
-      },
-      showMenu(i,status){
-        this.$refs.menuCollapsed.getElementsByClassName('submenu-hook-'+i)[0].style.display=status?'block':'none';
-      }
+    handleopen () {
+      // console.log('handleopen');
     },
-    mounted() {
-      var user = sessionStorage.getItem('user');
-      if (user) {
-        user = JSON.parse(user);
-        this.sysUserName = user.name || '';
-        this.sysUserAvatar = user.avatar || 'user';
-      }
+    handleclose () {
+      // console.log('handleclose');
+    },
+    handleselect: function (a, b) {
+    },
+    // 退出登录
+    logout: function () {
+      var _this = this
+      this.$confirm('确认退出吗?', '提示', {
+        // type: 'warning'
+      }).then(() => {
+        sessionStorage.removeItem('user')
+        _this.$router.push('/login')
+      }).catch(() => {
 
+      })
+    },
+    // 折叠导航栏
+    collapse: function () {
+      this.collapsed = !this.collapsed
+    },
+    showMenu (i, status) {
+      this.$refs.menuCollapsed.getElementsByClassName('submenu-hook-' + i)[0].style.display = status ? 'block' : 'none'
+    }
+  },
+  mounted () {
+    var user = sessionStorage.getItem('user')
+    if (user) {
+      user = JSON.parse(user)
+      this.sysUserName = user.name || ''
+      this.sysUserAvatar = user.avatar || 'user'
     }
   }
+}
 
 </script>
 
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,16 +1,16 @@
 <template>
-  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="left" label-width="0px" class="demo-ruleForm login-container">
     <h3 class="title">系统登录</h3>
     <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+      <el-input type="text" v-model="ruleForm.account" auto-complete="off" placeholder="账号"></el-input>
     </el-form-item>
     <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+      <el-input type="password" v-model="ruleForm.checkPass" auto-complete="off" placeholder="密码"></el-input>
     </el-form-item>
     <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
     <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
-      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit" :loading="logining">登录</el-button>
+      <!--<el-button @click.native.prevent="handleReset">重置</el-button>-->
     </el-form-item>
   </el-form>
 </template>
@@ -21,11 +21,11 @@ export default {
   data () {
     return {
       logining: false,
-      ruleForm2: {
+      ruleForm: {
         account: 'admin',
         checkPass: '123456'
       },
-      rules2: {
+      rules: {
         account: [
           { required: true, message: '请输入账号', trigger: 'blur' }
         ],
@@ -37,14 +37,14 @@ export default {
     }
   },
   methods: {
-    handleReset2 () {
-      this.$refs.ruleForm2.resetFields()
+    handleReset () {
+      this.$refs.ruleForm.resetFields()
     },
-    handleSubmit2 (ev) {
-      this.$refs.ruleForm2.validate((valid) => {
+    handleSubmit (ev) {
+      this.$refs.ruleForm.validate((valid) => {
         if (valid) {
           this.logining = true
-          var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass }
+          var loginParams = { username: this.ruleForm.account, password: this.ruleForm.checkPass }
           requestLogin(loginParams).then(data => {
             this.logining = false
             let { msg, code, user } = data
