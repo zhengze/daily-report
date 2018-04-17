202cbffdda247a7953ec2763681b214ac0ff2da6
zhengze <zhanghhd@163.com>
2018-04-17 19:12:02 +0800

    v-loading

--- src/pages/UserReport.vue
+++ src/pages/UserReport.vue
@@ -2,8 +2,8 @@
   <el-row class="container">
   <el-main>
     <el-row>
-    <el-col :span="24" class="toolbar">
-      <p>{{$route.params.userId}}
+    <el-col :span="24" class="toolbar" v-loading="loading">
+      <p>{{$route.query.cname}}
       <div class="myreport" v-for="(item, index) in reports.data" :key="index">
         <p><i class="fa fa-calendar-times-o"></i>{{item.created_time}}</p>
         <code>{{item.content}}</code>
@@ -33,18 +33,16 @@ export default {
       userId: this.$route.params.userId,
       reports: [],
       currentPage: 1,
-      pageSize: 10
+      pageSize: 10,
+      loading: true
     }
   },
   beforeRouteUpdate (to, from, next) {
     this.userId = to.params.userId
     next()
   },
-  beforeRouteEnter (to, from, next) {
-    this.getReports()
-  },
   methods: {
-    getReports: function () {
+    getReports () {
       let user = JSON.parse(sessionStorage.getItem('user'))
       let para = JSON.stringify({
         uid: user.id,
@@ -53,6 +51,7 @@ export default {
       })
       getReportList(para).then((res) => {
         this.reports = res.data.reportsList
+        this.loading = false
       })
     },
     handleCurrentChange (currentPage) {
@@ -76,16 +75,26 @@ export default {
         this.getReports()
       }
     },
-    '$route': 'getReports'
+    '$route': {
+      handler () {
+        this.getReports()
+      }
+    }
   }
 }
 </script>
 <style scoped lang="scss">
-.el-main {
-  top: 0px;
+.container {
+  position: absolute;
   overflow: auto;
-  .el-row {
-    overflow: auto;
+  top: 0px;
+  bottom: 0px;
+  .el-main {
+    width: 80%;
+    top: 0px;
+    .el-row {
+      text-align: left;
+    }
   }
 }
   
